9a7d44f392dceb198a30dbc8fd3f22fc9fde5cc5
badhon1512 <badhon1512@gmail.com>
2021-03-23 23:43:37 +0600

    get method API added

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Member;
 
 class MemberController extends Controller
 {
     //
+
+    function getMembers()
+    {
+          $members=Member::all();
+
+          if($members)
+          {
+              return $members;
+          }
+          else
+        {
+            return "Failed!!";
+        }
+    }
 }
